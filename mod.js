e91c38424f7d39ff26e2a09896e40d513db044ad
pedropedroaugusto968-arch <pedropedroaugusto968@gmail.com>
2026-01-22 15:21:58 -0300

    Create mod.js

---
+++ mod.js
@@ -0,0 +1,11 @@
+// FORÇAR LOGIN AO ABRIR
+(function() {
+    var senha = prompt("GSZIN MODS - DIGITE A KEY:");
+    if (senha === "trick4") {
+        alert("XIT DISFARÇADO ATIVADO!");
+        // O restante do seu código de mira aqui...
+    } else {
+        alert("KEY INCORRETA!");
+        location.reload();
+    }
+})();

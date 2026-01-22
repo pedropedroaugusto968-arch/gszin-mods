0f1c8acc4183d2c7ac7d1a0e78a0780645bad050
pedropedroaugusto968-arch <pedropedroaugusto968@gmail.com>
2026-01-22 15:31:06 -0300

    Add mod.js for key validation and alerts

---
+++ mod.js
@@ -0,0 +1,9 @@
+(function() {
+    alert("GSZIN MODS CARREGADO!");
+    var key = prompt("DIGITE A KEY:");
+    if (key === "trick4") {
+        alert("XIT DISFARÇADO ATIVADO!");
+    } else {
+        location.reload();
+    }
+})();

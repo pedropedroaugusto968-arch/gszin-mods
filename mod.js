3fcca301cbd42d64a496b14e14bd0fcaca616c80
pedropedroaugusto968-arch <pedropedroaugusto968@gmail.com>
2026-01-22 15:39:17 -0300

    Delete mod.js

--- mod.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    alert("GSZIN MODS CARREGADO!");
-    var key = prompt("DIGITE A KEY:");
-    if (key === "trick4") {
-        alert("XIT DISFARÇADO ATIVADO!");
-    } else {
-        location.reload();
-    }
-})();
